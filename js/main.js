dcccead3b9f1c465d4839d86e5efbc6b591e0c19
kyurururn <75336100+kyurururn@users.noreply.github.com>
2022-10-04 00:49:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+/*
+
+window.scrollBy(0,screen.height/2);
+
+console.log(body.clientHeight)
+console.log(window.innerHeight)
+*/
+
+let middle = document.getElementById("middle");
+middle.style.height = window.innerHeight * 0.9;
+
+let body = document.querySelector("body");
+body.style.height = window.innerHeight;
+
 let body = document.querySelector("body");
 let scrollTop = window.scrollY;
 body.style.top = (scrollTop - 1) + "px";
 body.classList.add("no_scroll");
-
-window.scrollBy(0,screen.height/2);
